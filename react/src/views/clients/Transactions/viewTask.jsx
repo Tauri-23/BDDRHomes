430b7a304af7694ca52948b6844095688405e513
Airich Jay Daiwan <airichjaydiawan@gmail.com>
2024-10-12 23:37:31 +0800

    update

--- react/src/views/clients/Transactions/viewTask.jsx
+++ react/src/views/clients/Transactions/viewTask.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { fetchTaskFullInfoById } from "../../../Services/GeneralTransactionService";
+import * as Icon from "react-bootstrap-icons";
+import "../../../assets/css/view_transaction.css";
 
 export default function ClientViewTask() {
     const {taskId} = useParams();
@@ -25,7 +27,23 @@ export default function ClientViewTask() {
             {task
             ? (
                 <>
+                    <div className="d-flex justify-content-start mar-bottom-1">
+                        <Link to={`/BDDRClient/ViewTransaction/${task.transaction}`} className="d-flex gap3 align-items-center text-l3 color-black1 text-decoration-none cursor-pointer">
+                            <Icon.ChevronLeft/>
+                            Back
+                        </Link>                
+                    </div>
                     <div className="text-l1 fw-bold color-black2 mar-bottom-l1">Task</div>
+
+                    <div className="d-flex gap2 mar-bottom-3 align-items-start">
+                        <div className="text-l1 fw-bold color-black2">{task.requirement}</div>
+                        <span className={`requirement-status-card ${task.status}`}>{task.status}</span>
+                    </div>
+                    <div className="text-m1 color-black2 mar-bottom-l3">{task.description}</div>
+
+                    <div className="d-flex">
+                        <div className="secondary-btn-black1 d-flex align-items-center gap4"><Icon.Paperclip/> Add File</div>
+                    </div>
                 </>
             )
             : (
